d0d90945e525c26dba6717baca781600e0778844
Rachel Manning <r@rachelmanning.com>
2019-12-04 22:09:48 -0600

    complete oojs starter

---
+++ app.js
@@ -0,0 +1,29 @@
+
+    // Create Dino Constructor
+
+
+    // Create Dino Objects
+
+
+    // Create Human Object
+
+    // Use IIFE to get human data from form
+
+
+    // Create Dino Compare Method 1
+
+
+    // Create Dino Compare Method 2
+
+    
+    // Create Dino Compare Method 3
+
+
+    // Generate Tiles for each Dino in Array
+  
+        // Add tiles to DOM
+
+    // Remove form from screen
+
+
+// On button click, prepare and display infographic
