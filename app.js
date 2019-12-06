7d2a28bb76a42453f475daa4373cb8b972cc68e7
Rachel Manning <r@rachelmanning.com>
2019-12-06 16:51:46 -0600

    update comments on app.js

--- app.js
+++ app.js
@@ -11,12 +11,15 @@
 
 
     // Create Dino Compare Method 1
+    // NOTE: Weight in JSON file is in lbs, height in inches. 
 
-
+    
     // Create Dino Compare Method 2
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
     
     // Create Dino Compare Method 3
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
