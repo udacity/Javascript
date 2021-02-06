ccb14c4fd9f5df292c0e6138c0846e85e83c9958
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-05 16:09:26 -0800

    Copied the JSON data into the app.js file and created a constructor function for Dino.

--- app.js
+++ app.js
@@ -1,6 +1,87 @@
-
+const dinos = [
+    {
+        species: "Triceratops",
+        weight: 13000,
+        height: 114,
+        diet: "herbavor",
+        where: "North America",
+        when: "Late Cretaceous",
+        fact: "First discovered in 1889 by Othniel Charles Marsh"
+    },
+    {
+        species: "Tyrannosaurus Rex",
+        weight: 11905,
+        height: 144,
+        diet: "carnivor",
+        where: "North America",
+        when: "Late Cretaceous",
+        fact: "The largest known skull measures in at 5 feet long."
+    },
+    {
+        species: "Anklyosaurus",
+        weight: 10500,
+        height: 55,
+        diet: "herbavor",
+        where: "North America",
+        when: "Late Cretaceous",
+        fact: "Anklyosaurus survived for approximately 135 million years."
+    },
+    {
+        species: "Brachiosaurus",
+        weight: 70000,
+        height: 372,
+        diet: "herbavor",
+        where: "North America",
+        when: "Late Jurasic",
+        fact: "An asteroid was named 9954 Brachiosaurus in 1991."
+    },
+    {
+        species: "Stegosaurus",
+        weight: 11600,
+        height: 79,
+        diet: "herbavor",
+        where: "North America, Europe, Asia",
+        when: "Late Jurasic to Early Cretaceous",
+        fact: "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+    },
+    {
+        species: "Elasmosaurus",
+        weight: 16000,
+        height: 59,
+        diet: "carnivor",
+        where: "North America",
+        when: "Late Cretaceous",
+        fact: "Elasmosaurus was a marine reptile first discovered in Kansas."
+    },
+    {
+        species: "Pteranodon",
+        weight: 44,
+        height: 20,
+        diet: "carnivor",
+        where: "North America",
+        when: "Late Cretaceous",
+        fact: "Actually a flying reptile, the Pteranodon is not a dinosaur."
+    },
+    {
+        species: "Pigeon",
+        weight: 0.5,
+        height: 9,
+        diet: "herbavor",
+        where: "World Wide",
+        when: "Holocene",
+        fact: "All birds are living dinosaurs."
+    }
+];
     // Create Dino Constructor
-
+function Dino(species, weight, height, diet, where, when, fact) {
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+    }
 
     // Create Dino Objects
 
@@ -11,19 +92,19 @@
 
 
     // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
+
         // Add tiles to DOM
 
     // Remove form from screen
