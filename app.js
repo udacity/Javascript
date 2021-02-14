13130e11446ddb46ec46cc9fb835522e63450de5
sahebe <sahebeh@gmail.com>
2021-02-14 10:41:44 +0330

    load Dino obejects

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-
+document.body.onload=generateDiopSpecs;
     // Create Dino Constructor
+  function DinoConstructor (species,weight,height,diet,where,when,fact){
+      this.species=species;
+      this.weight=weight;
+      this.height=height;
+      this.diet=diet;
+      this.where=where;
+      this.when=when;
+      this.fact=[
+      fact,
+      '${this.species} lived in ${where}',
+      '${this.species} lived during ${when}',
+      ]
 
-
-    // Create Dino Objects
-
+  }
+    // Load Dino Objects form dino.json
+    let dinoSpecs=[];
+        async function generateDiopSpecs(){
+            const dataJson=await fetch ("./dino.json");
+            const spec=await dataJson.json();
+        }
+        dinoSpecs= spec.DinoConstructor.map(dino=>new DinoConstructor(dino))
 
     // Create Human Object
+    function generateHumanSpecs(){
+        return(function(){
+            let humanObjectdiet= document.getElementById('diet').value;
+
+        }
+        )
+    }
+
+
 
     // Use IIFE to get human data from form
 
