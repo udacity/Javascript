91ade7d05f17cff755a9ad5b45e6cf771241bed4
benjaminhyw <benjaminhyw@gmail.com>
2021-07-30 16:41:07 -0700

    Remove comment

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ function loadJSON(callback) {
   xobj.open("GET", "dino.json", true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
-      // .open will NOT return a value but simply returns undefined in async mode so use a callback
       callback(xobj.responseText);
     }
   };
