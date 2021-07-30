ce64b40a7f85d564d033fbbd76af6c42b737102c
benjaminhyw <benjaminhyw@gmail.com>
2021-07-29 19:10:26 -0700

    Prettier format

--- app.js
+++ app.js
@@ -1,52 +1,47 @@
 // Create Dino Constructor
 function Dinosaur(dinoData) {
-    this.species = dinoData.species;
-    this.weight = dinoData.weight;
-    this.height = dinoData.height;
-    this.diet = dinoData.diet;
-    this.where = dinoData.where;
-    this.when = dinoData.when;
-    this.fact = dinoData.fact;
+  this.species = dinoData.species;
+  this.weight = dinoData.weight;
+  this.height = dinoData.height;
+  this.diet = dinoData.diet;
+  this.where = dinoData.where;
+  this.when = dinoData.when;
+  this.fact = dinoData.fact;
 }
 
 // Create Human Constructor
 function Human(humanData) {
-    this.name = humanData.name;
-    this.height = humanData.height;
-    this.weight = humanData.weight;
-    this.diet = humanData.diet;
+  this.name = humanData.name;
+  this.height = humanData.height;
+  this.weight = humanData.weight;
+  this.diet = humanData.diet;
 }
 
-    // Create Dino Objects
+// Create Dino Objects
 
-    // Create Human Object
+// Create Human Object
 
+// Use IIFE to get human data from form
+const getHumanData = () => {};
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Generate Tiles for each Dino in Array
 
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Add tiles to DOM
 
-
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
 const compareButtonHandler = (event) => {
-    console.log("Button was clicked")
-}
+  console.log("Button was clicked");
+};
 const compareButton = document.getElementById("btn");
-compareButton.addEventListener('click', compareButtonHandler);
+compareButton.addEventListener("click", compareButtonHandler);
