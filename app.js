7c3df4092236b14dada8772eca2e10be7d2cbe20
Andrea Kreideweiss <akreideweiss@gmail.com>
2021-08-23 10:02:51 +0200

    Create array of dinosaurs

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-
     // Create Dino Constructor
+    function Dinosaur(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+        }
 
 
     // Create Dino Objects
+    // go through json array and make a dinosaur each
+    const allDinosaurs = [];
+    // const dinoData = require("dino.json");
+    const dinoData = fetch('dino.json').then(response => response.json()).then(data => console.log(data));
+    const createDinoObjects = dinoData.Dinos.map(dino => new Dinosaur(dino.species, dino.weight, dino.height, dino.diet, dino.where, dino.when, dino.fact));
+    console.log(createDinoObjects);
 
 
     // Create Human Object
+    function Human(name, weight, height, food) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.food = food;
+    }
 
     // Use IIFE to get human data from form
 
